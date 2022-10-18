fa6ee83314bd7f754fdb3e9d99ab7db8066e6efb
allait330481 <330481@student.mboutrecht.nl>
2022-10-18 10:31:40 +0200

    alles af

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -1,17 +1,1 @@
-<p>
-<h3><?= $data["title"]; ?></h3>
-</p>
-<table>
-  <thead>
-    <tr>
-      <th>Naam</th>
-      <th>Vermogen</th>
-      <th>Leeftijd</th>
-      <th>Bedrijf</th>
-      <th>Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?= $data["richestPeoples"]; ?>
-  </tbody>
-</table>
+<a href="<?= URLROOT; ?>/RichestPeoples/index">RichestPeoples</a>
